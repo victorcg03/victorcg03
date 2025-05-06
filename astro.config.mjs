a27e099359526c7ac907b55f4c07b697eea928b0
victorcg03 <victorjosecorralguillot@gmail.com>
2025-05-06 20:50:59 +0200

    feat: add @astrojs/starlight dependency and update index.astro page
    - Added @astrojs/starlight to dependencies in package.json.
    - Translated index.astro page to Spanish and updated content.
    - Improved layout and styling using Tailwind CSS for better user experience.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 
+import starlight from "@astrojs/starlight";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), starlight()],
   vite: {
     plugins: [tailwindcss()],
   },
