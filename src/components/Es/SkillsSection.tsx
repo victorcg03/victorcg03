a77d96c8fbc89658230f02fd0d11ab1454992bad
victorcg03 <victorjosecorralguillot@gmail.com>
2025-05-16 19:09:25 +0200

    fix: traducir el título de habilidades al español

--- src/components/Es/SkillsSection.tsx
+++ src/components/Es/SkillsSection.tsx
@@ -53,7 +53,7 @@ export default function SkillsSection() {
       <div className="container max-w-4xl mx-auto px-6 md:px-4">
         <MotionWrapper>
           <h2 className="text-2xl font-bold mb-8 text-center md:text-left">
-            🛠️ Skills
+            🛠️ Habilidades
           </h2>
         </MotionWrapper>
 
